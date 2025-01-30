66a4e5acce8f664bba4afb1ea9edd23a22a558bc
Al101020 <alex.vl.m@yandex.ru>
2025-01-30 16:01:04 +1000

    commit-4

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -1,46 +1,56 @@
-// import "./board.css";
+import './board.css';
 
 // console.log('board.js')
 
 function createBoard() {
   const divBoard = document.createElement('div');
-  divBoard.style.display = 'flex';
-  divBoard.style.backgroundColor = 'blue';
-  divBoard.style.marginLeft = 'auto';
-  divBoard.style.marginRight = 'auto';
-  divBoard.style.width = '600px';
-  divBoard.style.height = '200px';
-  divBoard.style.paddingLeft = '3px';
-  divBoard.style.paddingRight = '3px';
-
   divBoard.classList.add('board');
-  // document.body.appendChild(divBoard);
 
-  const divСolumn1 = document.createElement('div');
-  divСolumn1.style.display = 'flex';
-  divСolumn1.style.backgroundColor = '#FFFFFF';
-  divСolumn1.style.margin = 'auto';
-  divСolumn1.style.width = '193px';
-  divСolumn1.style.height = '190px';
-  divСolumn1.classList.add('board');
+  const divСolumn1 = document.createElement('div'); //
+  divСolumn1.classList.add('divСolumn1');
+  const todo = document.createElement('p');
+  todo.classList.add('title');
+  todo.textContent = 'TODO';
+  divСolumn1.appendChild(todo); // ---
+  const block1 = document.createElement('div');
+  block1.classList.add('block');
+  divСolumn1.appendChild(block1); // --
+  const addLink1 = document.createElement('p');
+  addLink1.classList.add('addLink');
+  addLink1.textContent = '+ Add Another card';
+  divСolumn1.appendChild(addLink1);
   divBoard.appendChild(divСolumn1);
 
-  const divСolumn2 = document.createElement('div');
-  divСolumn2.style.display = 'flex';
-  divСolumn2.style.backgroundColor = '#FFFFFF';
-  divСolumn2.style.margin = 'auto';
-  divСolumn2.style.width = '193px';
-  divСolumn2.style.height = '190px';
-  divСolumn2.classList.add('board');
+  const divСolumn2 = document.createElement('div'); //
+  divСolumn2.classList.add('divСolumn2');
+  const inProgress = document.createElement('p');
+  inProgress.classList.add('title');
+  inProgress.textContent = 'IN PROGRESS';
+  divСolumn2.appendChild(inProgress); // ---
+  const block2 = document.createElement('div');
+  block2.classList.add('block');
+  divСolumn2.appendChild(block2); // --
+  const addLink2 = document.createElement('p');
+  addLink2.classList.add('addLink');
+  addLink2.textContent = '+ Add Another card';
+  divСolumn2.appendChild(addLink2);
+
   divBoard.appendChild(divСolumn2);
 
-  const divСolumn3 = document.createElement('div');
-  divСolumn3.style.display = 'flex';
-  divСolumn3.style.backgroundColor = '#FFFFFF';
-  divСolumn3.style.margin = 'auto';
-  divСolumn3.style.width = '193px';
-  divСolumn3.style.height = '190px';
-  divСolumn3.classList.add('board');
+  const divСolumn3 = document.createElement('div'); //
+  divСolumn3.classList.add('divСolumn3');
+  const done = document.createElement('p');
+  done.classList.add('title');
+  done.textContent = 'DONE';
+  divСolumn3.appendChild(done); // ---
+  const block3 = document.createElement('div');
+  block3.classList.add('block');
+  divСolumn3.appendChild(block3); // --
+  const addLink3 = document.createElement('p');
+  addLink3.classList.add('addLink');
+  addLink3.textContent = '+ Add Another card';
+  divСolumn3.appendChild(addLink3);
+
   divBoard.appendChild(divСolumn3);
 
   document.body.appendChild(divBoard);
