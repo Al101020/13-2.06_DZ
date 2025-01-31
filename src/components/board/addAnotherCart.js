240452d1962823651ecfe591fbd00460858fc9e5
Al101020 <alex.vl.m@yandex.ru>
2025-01-31 14:14:31 +1000

    commit-7

--- src/components/board/addAnotherCart.js
+++ src/components/board/addAnotherCart.js
@@ -1,22 +1,18 @@
 const addAnotherCart = document.querySelectorAll('.addLink');
-// console.log(addAnotherCart);
 
 let numerCart = 0;
 
-for (let i = 0; i < addAnotherCart.length; i += 1) { // console.log(addAnotherCart[i]);
-  addAnotherCart[i].addEventListener('click', () => {
-    /// onsole.log('click');
-    const columns = document.querySelectorAll('.column');
-    // console.log(columns[i]);
-    console.log(`колонка - ${[i + 1]}`);
+function addAnotherCartClick(e) {
+  const block = e.target.parentElement.querySelector('.block');
+  const cart = document.createElement('div');
+  cart.classList.add('cart');
+  numerCart += 1;
+  cart.textContent = `Карточка №${numerCart}`;
+  block.appendChild(cart);
+}
 
-    let cart = document.createElement('div');
-    cart.classList.add('cart');
-    numerCart += 1;
-    cart.textContent = 'Карточка №' + numerCart;
-    // console.log(numerCart);
-    // let temp = columns[i].querySelector('.block');
-    // console.log(temp);
-    columns[i].querySelector('.block').appendChild(cart);
+for (let i = 0; i < addAnotherCart.length; i += 1) {
+  addAnotherCart[i].addEventListener('click', (e) => {
+    addAnotherCartClick(e);
   });
 }
