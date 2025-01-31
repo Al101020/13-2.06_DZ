963a93c010382a99fe6422a82d9b0444917bbef9
Al101020 <alex.vl.m@yandex.ru>
2025-01-31 17:19:27 +1000

    commit-8

--- src/components/board/addAnotherCart.js
+++ src/components/board/addAnotherCart.js
@@ -4,11 +4,38 @@ let numerCart = 0;
 
 function addAnotherCartClick(e) {
   const block = e.target.parentElement.querySelector('.block');
+
   const cart = document.createElement('div');
   cart.classList.add('cart');
   numerCart += 1;
-  cart.textContent = `Карточка №${numerCart}`;
+
+  // cart.textContent = `Карточка №${numerCart}`;
+  const p = document.createElement('p');
+  p.textContent = `Карточка №${numerCart}`;
+  p.classList.add('text');
+  cart.appendChild(p);
+
+  const inputText = document.createElement('input');
+  inputText.type = 'text';
+  // console.log(inputText);
+
+  cart.appendChild(inputText);
+
   block.appendChild(cart);
+
+  // console.log(block.children.querySelector('input'));
+  inputText.addEventListener('blur', () => {
+    const divText = document.createElement('div');
+    divText.textContent = inputText.value;
+    cart.appendChild(divText);
+    cart.removeChild(inputText);
+
+    // console.log(inputText.value);
+    // cart.removeChild(inputText);
+    // console.log(e);
+
+    // console.log(e.target.parentElement); // .querySelector('input')
+  });
 }
 
 for (let i = 0; i < addAnotherCart.length; i += 1) {
--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,43 @@
 
 import '../components/board/board';
 import '../components/board/addAnotherCart';
+
+const board = document.querySelector('.board');
+// console.log(board);
+board.addEventListener('mousedown', (e) => {
+  // console.log('Клик в поле BOARD');
+  const carts = document.querySelectorAll('.cart');
+  // console.log(carts.length);
+  let actualCart;
+  if (carts.length === 0) {
+    // console.log('нет карточек');
+    return;
+  }
+  // console.log(carts.length);
+  // console.log(e.target);
+  if (e.target.classList.contains('cart')) {
+    console.log(`здесь cart есть, всего - ${carts.length}`);
+    actualCart = e.target;
+  } else if (e.target.parentElement.classList.contains('cart')) {
+    console.log(`У родителя cart есть, всего - ${carts.length}`);
+    actualCart = e.target.parentElement;
+  } else {
+    // console.log('Класса cart нет');
+  }
+  console.log(actualCart);
+
+  // if (carts.length == 0) {
+  //     console.log('нет карточек');
+  //     return;
+  // } else {
+  //     console.log(carts.length);
+  //     console.log(e.target);
+  //     if (e.target.classList.contains('cart')) {
+  //         console.log('здесь cart есть, всего - ' + carts.length);
+  //     } else if (e.target.parentElement.classList.contains('cart')) {
+  //         console.log('У родителя cart есть, всего - ' + carts.length);
+  //     } else {
+  //         console.log('Класса cart нет');
+  //     }
+  // };
+});
